c7da23f5530929316d545fe24b506dc3a0ebab6b
Alan Pitanga <alansantosbahia@gmail.com>
2024-09-09 19:32:37 -0300

    create project

---
+++ CalcIMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcIMC
+//
+//  Created by Alan Pitanga on 09/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
