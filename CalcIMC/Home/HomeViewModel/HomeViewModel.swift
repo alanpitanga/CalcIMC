9c9d6e3e3c7c6ac975efc836d4fbb31167da1b4d
Alan Pitanga <alansantosbahia@gmail.com>
2024-10-09 23:22:23 -0300

    Create ViewModel

--- CalcIMC/Home/HomeViewModel/HomeViewModel.swift
+++ CalcIMC/Home/HomeViewModel/HomeViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct HomeViewModel {
+    let title: String
+    let description: String
+    let weightTitle: String
+    let heightTitle: String
+    var weight: String
+    var height: String
+    let button: String
+    let titleResult: String
+    let result: String
+    var image: String
+}
